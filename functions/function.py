473c5c21d7e5332fa82c61b82f15f7b0a4c8afc9
Boburbro <boburotaboyev0@gmail.com>
2023-06-27 15:27:50 +0500

    TCT-TechCodingTeam
    data qo'shildi

--- functions/function.py
+++ functions/function.py
@@ -1,5 +1,33 @@
 from data.config import ADMINS
-def isAdmin(chat_id: str) -> str:
+def isAdmin(chat_id: str):
     """Admin yoki odiy userligini aniqlaydi"""
     if str(chat_id) in ADMINS:return True
     else:return False
+def isNewMemeber(chat_id: str):
+    """Yangi yoki eski user ligini aniqlaydi"""
+    import sqlite3
+    db = sqlite3.connect('baza.db')
+    sql = db.cursor()
+    rows = sql.execute("SELECT * FROM users")
+    users = []
+    for row in rows:
+        users.append(str(row[0]))
+    db.commit()
+    db.close()
+    if str(chat_id) in users:
+        return False
+    else:
+        return True
+
+def addNewMember(chat_id: str):
+    """Yangi userni bazaga saqlaydi"""
+    try:
+        import sqlite3
+        db = sqlite3.connect('baza.db')
+        sql = db.cursor()
+        sql.execute(f"INSERT INTO users VALUES ('{chat_id}')")
+        db.commit()
+        db.close()
+        return True
+    except:
+        return False
--- handlers/users/command_start.py
+++ handlers/users/command_start.py
@@ -1,10 +1,16 @@
 from loader import bot 
 from telebot.types import Message
-from functions.function import isAdmin
+from functions.function import isAdmin, isNewMemeber, addNewMember
 
 @bot.message_handler(commands=['start'])
 async def command_start(message: Message):
     if isAdmin(message.chat.id):
         await bot.send_message(chat_id=message.chat.id, text=f'Assalomu alaykum! Admin')
     else:
-        await bot.send_message(chat_id=message.chat.id, text=f'Assalomu alaykum! {message.from_user.full_name}')
+        if isNewMemeber(message.chat.id):
+            if addNewMember(message.chat.id):
+                await bot.send_message(chat_id=message.chat.id, text=f'Assalomu alaykum! {message.from_user.full_name}\nSiz botimizda yangisiz!')
+            else:
+                await bot.send_message(chat_id=message.chat.id, text='/start')
+        else:
+            await bot.send_message(chat_id=message.chat.id, text=f'Assalomu alaykum! {message.from_user.full_name}\nSiz botimizda eskisiz!')
